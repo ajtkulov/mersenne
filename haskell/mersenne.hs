9ca1838a078fe6d30b8a9171dfe7f5c691b9fde1
Pavel Ajtkulov <pavel.ajtkulov@tinypass.com>
2015-04-08 03:26:43 +0400

    -

--- haskell/mersenne.hs
+++ haskell/mersenne.hs
@@ -1,4 +1,5 @@
 import Data.Set as Set
+import Data.Hashable as H
 
 mer p = 2^p - 1
 
@@ -7,8 +8,19 @@ next1 x p = (x ^ 2 - 2 + p) `mod` p
 
 
 seq1 :: Integral a => a -> a -> Set a -> Set a
-seq1 x p s = if (member x s) 
+seq1 x p s = if (Set.member x s) 
        	   then
 		s
            else 
- 	   	seq1 (next1 x p) p (insert x s)
+ 	   	seq1 (next1 x p) p (Set.insert x s)
+
+cycleLength :: (Hashable a, Num a) => a -> (a -> a) -> Set Int -> a -> a
+cycleLength elem func set len = if (Set.member h set)
+	    	   	    	then
+					len
+				else
+					cycleLength newElem func (Set.insert h set) (len + 1)
+				where
+					h = H.hash elem
+					newElem = func elem
+					newHash = H.hash newElem
